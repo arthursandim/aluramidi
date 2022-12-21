9d3649773ff0bb5f3e8923b1604b86b5b12c2268
Arthur Sandim <arthur_gouvea@live.com>
2022-12-21 13:14:42 -0300

    aula 4 - uso de referencias e funções anonimas

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
-}
-
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
